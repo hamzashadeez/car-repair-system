e7404f6680238a72f0e96a7f2b4a439bda40da1a
hamzashadeez <hahmad1178@gmail.com>
2021-04-01 10:29:22 +0100

    Added a chart

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+function Chart() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Chart;
