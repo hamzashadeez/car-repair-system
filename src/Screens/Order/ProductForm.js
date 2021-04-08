c67fc280f6d0ee6c65dde9529e63e4cddbdbfa75
hamzashadeez <hahmad1178@gmail.com>
2021-04-08 18:48:48 +0100

    Added a product to select

--- src/Screens/Order/ProductForm.js
+++ src/Screens/Order/ProductForm.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './styles.css'
+import {db} from  '../../firebase'
+
 
 function ProductForm() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        (async () => {
+          db.collection("inventory").onSnapshot((snapshot) => {
+            setProducts(
+              snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+              }))
+            );
+          });
+        })();
+    },[])
     return (
         <div>
             <div className='p_div'>
@@ -9,8 +25,9 @@ function ProductForm() {
                 <form>
                 <label className='labelStyle'>Select Product</label>
                 <select className='myselect2'>
-                    <option>Glass</option>
-                    <option>Door</option>
+                {products.map(({ id, data }) => (
+                  <option key={id}>{data.name}</option>
+                ))}
                 </select>
                 <label className='labelStyle'>Enter Product Quantity needed</label>
                 <input className='myselect2' placeholder='quantity' type='number'/>
