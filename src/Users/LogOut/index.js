41e4ca11a105a85130cd690c6cc906da69a8d66f
hamzashadeez <hahmad1178@gmail.com>
2021-04-09 13:34:19 +0100

    Made log out UI

--- src/Users/LogOut/index.js
+++ src/Users/LogOut/index.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import './style.css'
-
+import React from "react";
+import "./style.css";
+import { useHistory } from "react-router-dom";
 function LogOutMech() {
-    return (
-        <div className='screenn'>
-           Log Out 
+  const history = useHistory();
+  const logout = ()=>{
+    //   log out
+  }
+  return (
+    <div className="screenn">
+      <div className="main_log">
+        <div className="logOutMech shadow">
+          <p>Want to Log out ?</p>
+          <div className="mt-3">
+            <button className="btn btn-success mr-2" onClick={()=>history.push('/')}>No, Go back</button>
+            <button className="btn btn-danger" onClick={()=>logout()}>Yes, Log Out</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default LogOutMech
+export default LogOutMech;
