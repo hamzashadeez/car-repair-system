18c9574e22bd5ed58806f80e62068ecedb15a556
hamzashadeez <hahmad1178@gmail.com>
2021-04-25 10:39:46 +0100

    Added a calendar

--- src/Screens/Schedule/index.js
+++ src/Screens/Schedule/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function Schedule() {
+    const [value, setValue] = useState(new Date())
     return (
         <div className="screen">
-            Schedule
+            <h5>Schedule</h5>
+            <div className='calen'>
+            <Calendar
+                onChange={setValue}
+                value={value}
+                />
+                <div className='schedule'>
+                    <div className='Day'>{value.toString()}</div>
+                    <h4 className='no_schedule'>
+                        No any schedule for today
+                    </h4>
+                </div>
+            </div>
         </div>
     )
 }
